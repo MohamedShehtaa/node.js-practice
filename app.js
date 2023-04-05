fe790ceac6729d48a580914dd1f50300793b6ca9
MohamedShehtaa <mmshehta92@gmail.com>
2023-04-05 15:59:33 +0200

    parse the req and add routing middleware and filtered with http methods

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
-const http = require ('http')
 const express = require('express')
+const bodyParser = require('body-parser')
+
 
 const app = express()
 
-app.use((req,res,next) => {
-    console.log("from moddleware no 1")
+app.use(bodyParser.urlencoded({extended:false}))
+
+app.use('/',(req,res,next) => { //default
+    // console.log("from moddleware no 1")
     next() // allow the req to move to next middleware
 })
+app.use('/add-product',(req,res,next) => {
+    res.send('<form action="/product" method="POST"> <input type="text" name="title" > <button type="submit" >add product </button></form>')
+})
+app.get('/product',(req,res,next) => {
+    console.log(req.body.title)
+    res.redirect('/')
+})
 
-app.use((req,res,next) => {
-    console.log("from moddleware no 2 ")
+app.use('/',(req,res,next) => {
+    // console.log("from moddleware no 2 ")
     res.send('<h3>hello from express </h3>')
 })
 
 const port = 3000
 
-app.listen(port,(error) =>{ console.log(`running in port ${port}`)})
+app.listen(port,(error) =>{ console.log(`running on  http://localhost:${port}/ `)})
