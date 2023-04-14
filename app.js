2b7f3e5fb25ad1ed1849e4eede3c6a98cdf159ab
MohamedShehtaa <mmshehta92@gmail.com>
2023-04-15 01:23:30 +0200

    update the response to send file instead text

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express')
 const bodyParser = require('body-parser')
 
@@ -15,7 +16,7 @@ app.use('/admin',adminRouter)
 app.use('/shop',shopRouter)
 // add 404 page in case un an handled requests  
 app.use((req,res,next)=>{
-    res.status(404).send('<h1> Page Not Found </h1>')
+    res.status(404).sendFile(path.join(__dirname,'views','not-found.html'))
 })
 
 
--- routes/shop.js
+++ routes/shop.js
@@ -1,9 +1,15 @@
+const path = require('path')
 const express = require('express')
 
 const router = express.Router()
 
 router.get('/',(req,res,next) => {
-    res.send(`<h3>hello from express  </h3>`)
+    // path.join will Join all arguments together and normalize the resulting path.  why join cuze will create path works on linux and windows systems
+    // - first params is dirname => refer to the folder of this file
+    // - second params is the folder you need to reach 
+    // - 3 params is the file name so on ... 
+    res.sendFile(path.join(__dirname,'../', 'views', 'shop.html'))  // method to send file to the client , it should be the absolute path
+
 })
 
 module.exports = router
