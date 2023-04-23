29469a9757b60c0760a0edb3f1783e77b8ad8046
MohamedShehtaa <mmshehta92@gmail.com>
2023-04-24 01:57:24 +0200

    dealing with the file storage to store data not array

--- models/product.js
+++ models/product.js
@@ -1,16 +1,35 @@
 // single product not products cuze the my app core is product 
 // so i want here to describe the structure of this product 
-const products = []
+const fs = require('fs')
+const path = require('path')
+
+const rootPath = require('../util/path')
+
 module.exports = class Product {
     constructor(t) {
         this.title = t
     }
 
     save() {
-        products.push(this)
+        const productPath = path.join(rootPath, 'data','products.json')
+        fs.readFile(productPath,(err,fileContent) => {
+            let products =[]
+            if(!err) {
+                products = JSON.parse(fileContent)
+            }
+            products.push(this)
+            fs.writeFile(productPath,JSON.stringify(products),(err)=> console.log(err))
+        })
     }
 
-    static fetchAll(){
-        return products
-    }
+    static fetchAll(cb){
+        const productPath = path.join(rootPath, 'data','products.json')
+        fs.readFile(productPath,(err,fileContent) => {
+            if(err) {
+                cd([])
+            }
+            cb(JSON.parse(fileContent))
+      
+    })
+   }
 }
