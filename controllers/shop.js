bd0fb3f803d9ff76801f4d3fbf5c38eeb1c75894
MohamedShehtaa <mmshehta92@gmail.com>
2023-05-21 18:00:10 +0300

    update shop js

--- controllers/shop.js
+++ controllers/shop.js
@@ -58,10 +58,33 @@ exports.getCart = (req, res, next) => {
 
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.findById(prodId, product => {
-    Cart.addProduct(prodId, product.price);
-  });
-  res.redirect('/cart');
+  let fetchedCart ;
+ req.user
+    .getCart()
+    .then(cart => {
+        fetchedCart = cart
+        return cart.getProducts({where:{id:prodId}})
+    })
+    .then(products => {
+        let product 
+        if(products.length >0) {
+            product = products[0]
+        }
+        let newQuantity = 1
+        if(product) {
+            //..
+        }
+        return Product.findOne({where:{id:prodId}}).then(product =>{
+            console.log(product)
+            return fetchedCart.addProduct(product,{through:{quantity:newQuantity}})
+        })
+        .catch(e=>console.log(e))
+
+    })
+    .then(()=>{
+        res.redirect('/cart');
+    })
+    .catch(err=>console.log(err))
 };
 
 exports.postCartDeleteProduct = (req, res, next) => {
